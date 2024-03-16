3e3a5ea3d55e7149803822ec5374e9dc557a6db5
Ansh Vardhan Jasrotia <anshvardhanavj@gmail.com>
2024-03-16 17:43:04 +0530

    added &apos to menu file

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
   return (
     <div className={styles.container}>
 
-      <h2 className={styles.subtitle}>{`What's hot`}</h2>
+      <h2 className={styles.subtitle}>What&apos;s hot</h2>
       <h1 className={styles.title}>Most Poplular</h1>
       <MenuPosts withImage={false}/>
     
