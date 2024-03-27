f6052c84f7ef3e978ab6865b99519542a25c8647
Ansh Jasrotia <anshvardhanavj@gmail.com>
2024-03-27 14:13:50 +0530

    fixed errors

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,4 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
-// import { Prisma } from "@prisma/client"
-import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import prisma from "./connect"
 import { getServerSession } from "next-auth"
@@ -12,10 +10,7 @@ export const authOptions = {
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
         }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
+        
     ],
 }
 
