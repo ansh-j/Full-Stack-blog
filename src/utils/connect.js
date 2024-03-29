7e25b12e570d8ae510f9669e89ed7f5c36472b73
Ansh Jasrotia <anshvardhanavj@gmail.com>
2024-03-29 13:09:00 +0530

    prisma error

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 let prisma
 if(process.env.NODE_ENV==='production'){
-    prisma== new PrismaClient()
+    prisma = new PrismaClient()
 }else{
     if(!global.prisma){
         global.prisma=new PrismaClient()
